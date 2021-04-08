35a295e1adf49d78ff6b360edb393e72707d78cd
Brandon Satrom <bsatrom+gh@gmail.com>
2021-04-08 13:55:05 -0500

    fix:change default usb air sample setting to 15

--- src/settingsStore.js
+++ src/settingsStore.js
@@ -3,7 +3,7 @@ import { writable } from 'svelte/store';
 export const deviceName = writable("");
 export const displayValue = writable("");
 export const indoorDevice = writable(false);
-export const sampleFrequencyUSB = writable("30");
+export const sampleFrequencyUSB = writable("15");
 export const sampleFrequencyFull = writable("30");
 export const sampleFrequencyLow = writable("720");
 export const contactName = writable("");
