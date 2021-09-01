3d38595d588e6ef61186d1c173b2a78e7334dfdc
TJ VanToll <tj.vantoll@gmail.com>
2021-09-01 16:23:30 -0400

    Hamburger menu for navigation

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script>
   import { Router, Route } from 'svelte-routing';
 
+  import CloseIcon from './icons/CloseIcon.svelte';
+  import MenuIcon from './icons/MenuIcon.svelte';
   import Settings from './routes/Settings/Settings.svelte';
   import Dashboard from './routes/Dashboard/Dashboard.svelte';
   import Home from './routes/Home/Home.svelte';
   import { getCurrentDeviceFromUrl } from './services/device';
 
   const currentDevice = getCurrentDeviceFromUrl(window.location);
+
+  let menuOpen = false;
+  const toggleMenu = () => menuOpen = !menuOpen;
 
   export let url = '';
   export let pin = currentDevice.pin;
@@ -20,7 +25,7 @@
       <img alt="Airnote logo" src="/images/airnote.svg" />
     </a>
     {#if deviceUID}
-      <ul>
+      <ul class="{menuOpen ? 'open' : ''}">
         <li>
           <a href="/{deviceUID}?product={productUID}&pin={pin}">
             Settings
@@ -42,6 +47,10 @@
           </a>
         </li>
       </ul>
+      <button class="svg-button" on:click={toggleMenu}>
+        {#if menuOpen }<CloseIcon />{/if}
+        {#if !menuOpen }<MenuIcon />{/if}
+      </button>
     {/if}
   </header>
   <main>
