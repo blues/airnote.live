bf0b3eb05d2f6e190112b3abd1b4f271eb0b48c1
TJ VanToll <tj.vantoll@gmail.com>
2021-09-03 15:26:32 -0400

    Trying to move the aws lambda to Render—let's see what happens!

--- server/server.js
+++ server/server.js
@@ -7,6 +7,55 @@ const server = Hapi.server({
   host: "0.0.0.0" // needed for Render deployment
 });
 
+const body = {
+  "size": 500,
+  "sort": [
+    {
+      "service_uploaded": {
+        "order": "desc"
+      }
+    }
+  ],
+  "query": {
+    "bool": {
+      "must": [],
+      "filter": [
+        {
+          "bool": {
+            "should": [
+              {
+                "match_phrase": {
+                  "device_urn": "note:" + device_uid
+                }
+              }
+            ]
+          }
+        },
+        {
+          "range": {
+            "service_uploaded": {
+              "format": "strict_date_optional_time",
+              "gte": from,
+              "lte": to
+            }
+          }
+        }
+      ]
+    }
+  }
+};
+
+const options = {
+  hostname: '40ad140d461d810ac41ed710b5c7a5b6.us-west-2.aws.found.io',
+  port: 9243,
+  path: '/_search',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic ' + new Buffer(process.env.username + ':' + process.env.password).toString('base64')
+  }
+};
+
 const init = async () => {
   await server.register([
     {
@@ -17,16 +66,30 @@ const init = async () => {
       plugin: require('hapi-pino'),
       options: {
         prettyPrint: true,
-        logEvents: ["response", "onPostStart"]
+        logEvents: ['response', 'onPostStart']
       }
     }]);
 
   server.route({
-    method: "GET",
-    path: "/",
+    method: 'GET',
+    path: '/',
     handler: (request, h) => {
-      return 'Username: ' + process.env.username +
-        ' device_uid: ' + request.query.device_uid
+      return new Promise(function(resolve, reject) {
+        var req = https.request(options, res => {
+          var data = '';
+          res.on('data', chunk => {
+            data += chunk;
+          });
+          res.on('end', () => {
+            resolve({
+              statusCode: 200,
+              body: data
+            });
+          });
+        });
+        req.write(JSON.stringify(body));
+        req.end();
+      });
     }
   });
 
