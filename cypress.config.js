a7591a593cf50ed7ed94fa5af41dc38f63250fb3
Paige Niedringhaus <paigen11@gmail.com>
2022-08-10 10:15:45 -0400

    nonsense commit

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,5 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:5555",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
