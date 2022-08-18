0af596b87f06bcba24ae83ea253ed9466b3a5d85
Paige Niedringhaus <paigen11@gmail.com>
2022-08-18 11:41:45 -0400

    pushing changes for staging version

--- server/server.js
+++ server/server.js
@@ -69,7 +69,11 @@ const init = async () => {
     path: "/",
     options: {
       cors: {
-        origin: ["http://localhost:5555", "https://airnote.live"],
+        origin: [
+          "http://localhost:5555",
+          "https://airnote.live",
+          "https://airnote-live-pr-45.onrender.com",
+        ],
       },
       handler: async (request, h) => {
         const deviceUID = request.query.device_uid;
