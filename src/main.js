27d1e77fe54a9cec70c4df9e1466f93413c78fec
TJ VanToll <tj.vantoll@gmail.com>
2023-09-05 13:34:55 -0400

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.body,
-	hydrate: true
+  target: document.body,
+  hydrate: true,
 });
+
+console.log("test");
 
 export default app;
