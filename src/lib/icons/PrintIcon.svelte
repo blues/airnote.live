dfbe026d8ed2eae1aeb8dde7d5ea4c18e4a560a7
TJ VanToll <tj.vantoll@gmail.com>
2023-10-05 09:09:15 -0400

    feat: updated action icons and styling (#58)
    * feat: updated action icons and styling
    
    * color tweaks
    
    * Update src/routes/styles.css
    
    Co-authored-by: Paige Niedringhaus <paigen11@gmail.com>
    
    ---------
    
    Co-authored-by: Paige Niedringhaus <paigen11@gmail.com>

--- src/lib/icons/PrintIcon.svelte
+++ src/lib/icons/PrintIcon.svelte
@@ -1,14 +1,10 @@
 <svg
   xmlns="http://www.w3.org/2000/svg"
-  class="h-6 w-6"
-  fill="none"
+  height="24px"
   viewBox="0 0 24 24"
-  stroke="currentColor"
+  width="24px"
+  fill="currentColor"
+  ><path d="M0 0h24v24H0V0z" fill="none" /><path
+    d="M19 8H5c-1.66 0-3 1.34-3 3v4c0 1.1.9 2 2 2h2v2c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2v-2h2c1.1 0 2-.9 2-2v-4c0-1.66-1.34-3-3-3zm-4 11H9c-.55 0-1-.45-1-1v-4h8v4c0 .55-.45 1-1 1zm4-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-2-9H7c-.55 0-1 .45-1 1v2c0 .55.45 1 1 1h10c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1z"
+  /></svg
 >
-  <path
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    stroke-width="2"
-    d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
-  />
-</svg>
---
+++ src/routes/[deviceUID]/dashboard/Actions.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import { unparse } from 'papaparse';
+  import DownloadIcon from '$lib/icons/DownloadIcon.svelte';
+  import PrintIcon from '$lib/icons/PrintIcon.svelte';
+  import ShareIcon from '$lib/icons/ShareIcon.svelte';
+  import type { AirnoteReading } from '$lib/services/AirReadingModel';
+  import { shareDashboard } from '$lib/util/share';
+
+  export let data: AirnoteReading[];
+  export let deviceUID: string;
+
+  const downloadData = () => {
+    const csv = 'data:text/csv;charset=utf-8,' + unparse(data);
+    const encodedURI = encodeURI(csv);
+
+    var link = document.createElement('a');
+    link.setAttribute('href', encodedURI);
+    link.setAttribute('download', 'airnote.csv');
+    link.click();
+  };
+</script>
+
+<div class="actions">
+  <button on:click={downloadData}>
+    <DownloadIcon />
+    <span>Download</span>
+  </button>
+
+  <button on:click={() => window.print()}>
+    <PrintIcon />
+    <span>Print</span>
+  </button>
+
+  <button on:click={() => shareDashboard(deviceUID)}>
+    <ShareIcon />
+    <span>Share</span>
+  </button>
+</div>
+
+<style>
+  .actions {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.5rem;
+  }
+  .actions button {
+    background: transparent;
+    color: var(--notehubBlue);
+    font-size: 0.75rem;
+    padding: 0;
+  }
+  .actions button:hover {
+    color: var(--notehubBlueTintLighter);
+  }
+  .actions span {
+    padding-top: 6px;
+    display: block;
+  }
+</style>
