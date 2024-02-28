acf0cee0655f1bd922538d5c8cd7560011bb6811
Paige Niedringhaus <paigen11@gmail.com>
2024-02-28 15:09:54 -0500

    minor code cleanup

--- src/lib/ory/httpConfig.ts
+++ src/lib/ory/httpConfig.ts
@@ -1,8 +1,7 @@
 import { Configuration, FrontendApi } from '@ory/client';
 import { PUBLIC_ORY_SDK_URL } from '$env/static/public';
 
-// base path is set to 4000 because that's the default for Ory Tunnel: https://www.ory.sh/docs/cli/ory-tunnel
-// const basePath = 'http://localhost:4000';
+// PUBLIC_ORY_SDK_URL is set to http://localhost:4000 in local dev as that's the default for Ory Tunnel: https://www.ory.sh/docs/cli/ory-tunnel
 const basePath = PUBLIC_ORY_SDK_URL;
 
 export const oryFrontendApi = new FrontendApi(
