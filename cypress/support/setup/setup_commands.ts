bbfe67343dcba1adf3de73e3df5913185ef806f1
Paige Niedringhaus <paigen11@gmail.com>
2025-12-22 17:37:40 -0500

    tests: add new Cypress tests to check correct options for settings displayed in v1 vs v3 Airnotes

--- cypress/support/setup/setup_commands.ts
+++ cypress/support/setup/setup_commands.ts
@@ -10,3 +10,16 @@ export const setLocalStorage = () => {
     );
   });
 };
+
+export const setLocalStorageV3 = () => {
+  cy.window().then((win) => {
+    win.localStorage.setItem(
+      'device',
+      JSON.stringify({
+        deviceUID: 'dev:868531060199234',
+        pin: '1234',
+        productUID: 'product:com.blues.airnote.v3'
+      })
+    );
+  });
+};
